7545bae38992aee7492323911dcb4619ae1d06c5
Asko Kauppi <asko.kauppi@zalando.fi>
2016-02-25 14:40:21 +0200

    interim for people to comment

--- src/test/scala/ApproachA.scala
+++ src/test/scala/ApproachA.scala
@@ -3,6 +3,7 @@ package test
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializerSettings, SourceShape, ActorMaterializer}
 import akka.stream.scaladsl.{Flow, GraphDSL, Sink, Source}
+import com.softwaremill.react.kafka.{ReactiveKafka, ConsumerProperties}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.scalatest.{FlatSpec, Matchers}
 import org.apache.kafka.common.serialization._
@@ -19,6 +20,9 @@ import scala.concurrent.duration._
 *   -> https://github.com/softwaremill/reactive-kafka/blob/master/docs/NewAPI.md
 *   -> https://github.com/softwaremill/reactive-kafka/blob/master/core/src/main/scala/com/softwaremill/react/kafka2/DummyProducer.scala
 *   -> https://github.com/softwaremill/reactive-kafka/blob/master/core/src/main/scala/com/softwaremill/react/kafka2/DummyConsumer.scala
+*
+* Ref.
+*   Kafka Producer properties -> http://kafka.apache.org/documentation.html#producerconfigs
 */
 class ApproachATest extends FlatSpec with Matchers {
 
@@ -43,9 +47,14 @@ class ApproachATest extends FlatSpec with Matchers {
     val keySerializer = new ByteArraySerializer // Note: no idea why this, reactive-kafka 'DummyProducer' sample uses it
     val valSerializer = new IntegerSerializer
 
-    val producerSink = Producer.sink( ProducerProvider(host, keySerializer, valSerializer) )
+    val producerSink = Producer.sink(
+      ProducerProvider(host, keySerializer, valSerializer)
+        .props(
+          "acks" -> "1"                  // let Kafka server acknowledge receiving each entry
+        )
+    )
 
-    var count=0;
+    var count=0
 
     Source
       .fromIterator(() => data.toIterator)
@@ -54,7 +63,7 @@ class ApproachATest extends FlatSpec with Matchers {
       //.via(producer)                          // Q: this the place that actually writes to Kafka?
       //.mapAsync(1)(identity)                  // Q: what does this do? (it's an Akka Streams thing)
       //.to(shutdownAsOnComplete)               // clean up the actor system, when stream is ready
-      .map( x => { println(x.value); count++; x } )
+      .map( x => { println(x.value); count += 1; x } )
       .to(producerSink)
       .run()
 
@@ -72,50 +81,71 @@ class ApproachATest extends FlatSpec with Matchers {
     implicit val actorSystem = ActorSystem("ApproachA_read")
     implicit val materializer = ActorMaterializer(
       ActorMaterializerSettings(actorSystem)
-          .withAutoFusing(false)
+          //.withAutoFusing(false)
           .withInputBuffer(16,16)
     )
 
     val keyDeserializer = new ByteArrayDeserializer
     val valDeserializer = new IntegerDeserializer
 
-    val prov = ConsumerProvider(host, keyDeserializer, valDeserializer)
-      .setup(TopicSubscription(topic))
-      .groupId(consumerGroupId)
-      .autoCommit(false)
-      .props("auto.offset.reset" -> "earliest")   // tbd. what's the purpose of this?
-
     var received = new ArrayBuffer[Int]
 
-    val graph = GraphDSL.create(Consumer[Array[Byte], java.lang.Integer](prov)) { implicit b => kafka =>
-      import GraphDSL.Implicits._
-      type In = ConsumerRecord[Array[Byte], java.lang.Integer]
+    val kafka = new ReactiveKafka
 
-      val dummyProcessor = Flow[In].map { x =>    // tbd. probably way to make this without 'map' (i.e. run a 'Unit' producing function for its side effects)
-        val v: Int = x.value.toInt
-        Thread.sleep(1000)    // ms
-        println(v)
-        received += v
-        x
-      }
+    val consumerProps = ConsumerProperties(
+      bootstrapServers = "localhost:9092",    // NOT 'brokerList' like reactive-kafka README says
+      topic = topic,
+      groupId = consumerGroupId,
+      valueDeserializer = new StringDeserializer
+    )
+      .commitInterval(5 seconds)
 
-      kafka.messages ~> dummyProcessor ~> Consumer.record2commit ~> kafka.commit
-      SourceShape(kafka.confirmation)
-    }
+    val consumerWithOffsetSink = kafka.consumeWithOffsetSink(consumerProps)
 
-    val control =
-      Source.fromGraph(graph)
-        .mapAsync(8)(identity)
-        .to(shutdownAsOnComplete)
-        .run()
+    Source.fromPublisher(consumerWithOffsetSink.publisher)
+      .map( x => {
+        val v= x.value.toInt; println(v); received += v; x
+      })
+      .to(consumerWithOffsetSink.offsetCommitSink) // stream back for commit
+      .run()
 
-    sys.addShutdownHook {
-      control.stop()
+    /***
+      * val prov = ConsumerProvider(host, keyDeserializer, valDeserializer)
+      * .setup(TopicSubscription(topic))
+      * .groupId(consumerGroupId)
+      * .autoCommit(false)
+      * .props("auto.offset.reset" -> "earliest")   // tbd. what's the purpose of this?
 
-      println("Waiting for stop!")
-      Await.result(actorSystem.whenTerminated, 30.seconds)
-      println("AS stopped!")
-    }
+      * val graph = GraphDSL.create(Consumer[Array[Byte], java.lang.Integer](prov)) { implicit b => kafka =>
+      * import GraphDSL.Implicits._
+      * type In = ConsumerRecord[Array[Byte], java.lang.Integer]
+
+      * val dummyProcessor = Flow[In].map { x =>    // tbd. probably way to make this without 'map' (i.e. run a 'Unit' producing function for its side effects)
+      * val v: Int = x.value.toInt
+      * Thread.sleep(1000)    // ms
+      * println(v)
+      * received += v
+      * x
+      * }
+
+      * kafka.messages ~> dummyProcessor ~> Consumer.record2commit ~> kafka.commit
+      * SourceShape(kafka.confirmation)
+      * }
+
+      * val control =
+      * Source.fromGraph(graph)
+      * .mapAsync(8)(identity)
+      * .to(shutdownAsOnComplete)
+      * .run()
+
+      * sys.addShutdownHook {
+      * control.stop()
+
+      * println("Waiting for stop!")
+      * Await.result(actorSystem.whenTerminated, 30.seconds)
+      * println("AS stopped!")
+      * }
+    ***/
 
     received should contain theSameElementsInOrderAs(data)
   }
